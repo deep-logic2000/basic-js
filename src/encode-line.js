5b6d4d6b436361f9c11c9cce12392b940fad5577
Kirill Nazarov <deeplogic2000@gmail.com>
2023-04-14 19:31:34 +0300

    feat: encode-line function resolve

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,41 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+ let resStr = "";
+
+ let tempChar = str[0]; // c
+ let tempCharCount = 0; // 2
+
+ for (let i = 0; i < str.length; i++) {
+  debugger;
+  // 6 - a
+  if (str[i] === tempChar) {
+   tempCharCount++;
+   if (i === str.length - 1) {
+    resStr += `${tempCharCount}${tempChar}`;
+   }
+  } else {
+   if (i === str.length - 1) {
+    tempCharCount <= 1
+     ? (resStr += tempChar)
+     : (resStr += `${tempCharCount}${tempChar}`);
+    resStr += str[i];
+   } else {
+    tempCharCount <= 1
+     ? (resStr += tempChar)
+     : (resStr += `${tempCharCount}${tempChar}`);
+    tempChar = str[i];
+    tempCharCount = 1;
+
+   }
+  }
+ }
+
+ return resStr;
 }
 
 module.exports = {
-  encodeLine
+ encodeLine,
 };
+
